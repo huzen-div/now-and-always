c18374e435f1266808f3f1f8a297c4becf91f966
huzen-div <80887881+huzen-div@users.noreply.github.com>
2023-07-29 09:36:56 +0700

    comment

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from "vuex";
-import { auth } from "./auth.module";
-import { main } from "./main.module";
-const store = createStore({
-  modules: {
-    auth,main,
-  },
-});
-export default store;
